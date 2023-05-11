b4c45119e0139c607f81a819445fbe30654c9831
cogA <kennethtanmh99@gmail.com>
2023-05-11 15:58:17 +0800

    done

--- app/src/App.js
+++ app/src/App.js
@@ -1,69 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import './App.css';
-// import ForexTable from './ForexTable';
-
-// function App() {
-//   const [data, setData] = useState([]);
-
-//   // Replace the URL with the actual endpoint
-//   useEffect(() => {
-//     fetch('https://your-api-endpoint.com')
-//       .then(response => response.json())
-//       .then(json => setData(json))
-//       .catch(err => console.error('Error fetching data', err));
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Forex Exchange</h1>
-//       </header>
-//       <ForexTable data={data} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React from 'react';
-// import './App.css';
-// import ForexTable from './ForexTable';
-// import BoALogo from './boa_logo.png'; // Assuming your logo is in src folder
-
-// function App() {
-//   const data = [
-//     {
-//       "EventId": 3,
-//       "Ccy": "GBX",
-//       "Tenor": "1M",
-//       "Position": "NA",
-//       "Bid": "NA",
-//       "Ask": "NA",
-//       "QuoteStatus": "EXCEPTION"
-//     },
-//     {
-//       "EventId": 91,
-//       "Ccy": "GBX",
-//       "Tenor": "2M",
-//       "Position": "NA",
-//       "Bid": "NA",
-//       "Ask": "NA",
-//       "QuoteStatus": "EXCEPTION"
-//     }
-//   ];
-
-//   return (
-//     <div className="App">
-//       <div className="header-container">
-//         <img className="logo" src={BoALogo} alt="Bank of America Logo" />
-//       </div>
-//       <ForexTable data={data} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import './App.css';
 import ForexTable from './ForexTable';
@@ -80,6 +14,7 @@ function App() {
       "Ask": "NA",
       "QuoteStatus": "EXCEPTION"
     },
+    
     {
         "EventId": 25,
         "Ccy": "GBX",
@@ -160,7 +95,7 @@ function App() {
           <img className="logo" src={BoALogo} alt="Bank of America Logo" />
       </div>
       <div className="header-container">
-        <div className="header-title">Forex Exchange</div>
+        <div className="header-title">Forward Pricing Engine</div>
       </div>
       <div className="forex-table">
         <ForexTable data={data} />
@@ -170,5 +105,7 @@ function App() {
 }
 
 export default App;
+
+
 
 
--- app/src/ForexTable.js
+++ app/src/ForexTable.js
@@ -1,34 +1,297 @@
-import React from 'react';
+// import React from 'react';
+// import './ForexTable.css';
+
+// function ForexTable({ data }) {
+//   const getCellStyle = (quoteStatus) => {
+//     let color;
+//     switch (quoteStatus) {
+//       case 'TRADABLE':
+//         color = '#00A86B'; // Green color code
+//         break;
+//       case 'EXCEPTION':
+//         color = '#E61030'; // Red color code
+//         break;
+//       case 'NON-TRADABLE':
+//         color = '#FFA500'; // Orange color code
+//         break;
+//       default:
+//         color = '#000000'; // Default color code
+//         break;
+//     }
+//     return { backgroundColor: color, color: 'white' };
+//   };
+  
+
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Currency</th>
+//           <th>Tenor</th>
+//           <th>Position</th>
+//           <th>Bid</th>
+//           <th>Ask</th>
+//           <th>Status</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {data.map((event, index) => (
+//           <tr key={index}>
+//             <td>{event.Ccy}</td>
+//             <td>{event.Tenor}</td>
+//             <td>{event.Position}</td>
+//             <td>{event.Bid}</td>
+//             <td>{event.Ask}</td>
+//             <td style={getCellStyle(event.QuoteStatus)}>{event.QuoteStatus}</td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   );
+// }
+
+// export default ForexTable;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import './ForexTable.css';
+
+// function ForexTable({ data }) {
+//   const [filteredData, setFilteredData] = useState(data);
+//   const [showNAPosition, setShowNAPosition] = useState(true);
+//   const [showNABid, setShowNABid] = useState(true);
+//   const [showNAAsk, setShowNAAsk] = useState(true);
+//   const [statusFilter, setStatusFilter] = useState('');
+
+//   useEffect(() => {
+//     const filtered = data.filter((event) => {
+//       const statusMatch = statusFilter === '' || event.QuoteStatus === statusFilter;
+//       const showMatchPosition = showNAPosition || event.Position !== 'NA';
+//       const showMatchBid = showNABid || event.Bid !== 'NA';
+//       const showMatchAsk = showNAAsk || event.Ask !== 'NA';
+
+//       return statusMatch && showMatchPosition && showMatchBid && showMatchAsk;
+//     });
+
+//     setFilteredData(filtered);
+//   }, [data, statusFilter, showNAPosition, showNABid, showNAAsk]);
+
+//   const getCellStyle = (quoteStatus) => {
+//     let color;
+//     switch (quoteStatus) {
+//       case 'TRADABLE':
+//         color = '#00A86B'; // Green color code
+//         break;
+//       case 'EXCEPTION':
+//         color = '#E61030'; // Red color code
+//         break;
+//       case 'NON-TRADABLE':
+//         color = '#FFA500'; // Orange color code
+//         break;
+//       default:
+//         color = '#000000'; // Default color code
+//         break;
+//     }
+//     return { backgroundColor: color, color: 'white' };
+//   };
+
+//   return (
+//     <div>
+//       <div className="filters">
+//         <label>
+//           Status:
+//           <select
+//             value={statusFilter}
+//             onChange={(e) => setStatusFilter(e.target.value)}
+//           >
+//             <option value="">All</option>
+//             <option value="TRADABLE">Tradable</option>
+//             <option value="EXCEPTION">Exception</option>
+//             <option value="NON-TRADABLE">Non-Tradable</option>
+//           </select>
+//         </label>
+//         <label>
+//           Position:
+//           <select
+//             value={showNAPosition ? 'all' : 'filtered'}
+//             onChange={(e) => setShowNAPosition(e.target.value === 'all')}
+//           >
+//             <option value="all">All</option>
+//             <option value="filtered">Filtered</option>
+//           </select>
+//         </label>
+//         <label>
+//           Bid:
+//           <select
+//             value={showNABid ? 'all' : 'filtered'}
+//             onChange={(e) => setShowNABid(e.target.value === 'all')}
+//           >
+//             <option value="all">All</option>
+//             <option value="filtered">Filtered</option>
+//           </select>
+//         </label>
+//         <label>
+//           Ask:
+//           <select
+//             value={showNAAsk ? 'all' : 'filtered'}
+//             onChange={(e) => setShowNAAsk(e.target.value === 'all')}
+//           >
+//             <option value="all">All</option>
+//             <option value="filtered">Filtered</option>
+//           </select>
+//         </label>
+//       </div>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Currency</th>
+//             <th>Tenor</th>
+//             <th>Position</th>
+//             <th>Bid</th>
+//             <th>Ask</th>
+//             <th>Status</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {filteredData.map((event, index) => (
+//             <tr key={index}>
+//               <td>{event.Ccy}</td>
+//               <td>{event.Tenor}</td>
+//               <td>{showNAPosition ? event.Position : event.Position !== 'NA' ? event.Position : ''}</td>
+//               <td>{showNABid ? event.Bid : event.Bid !== 'NA' ? event.Bid : ''}</td>
+//               <td>{showNAAsk ? event.Ask : event.Ask !== 'NA' ? event.Ask : ''}</td>
+//               <td style={getCellStyle(event.QuoteStatus)}>{event.QuoteStatus}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+//           }
+
+//           export default ForexTable;
+
+
+
+
+import React, { useState, useEffect } from 'react';
 import './ForexTable.css';
 
 function ForexTable({ data }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          
-          <th>Currency</th>
-          <th>Tenor</th>
-          <th>Position</th>
-          <th>Bid</th>
-          <th>Ask</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((event, index) => (
-          <tr key={index}>
-            <td>{event.Ccy}</td>
-            <td>{event.Tenor}</td>
-            <td>{event.Position}</td>
-            <td>{event.Bid}</td>
-            <td>{event.Ask}</td>
-            <td>{event.QuoteStatus}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+  const [filteredData, setFilteredData] = useState(data);
+  const [showNAPosition, setShowNAPosition] = useState(true);
+  const [showNABid, setShowNABid] = useState(true);
+  const [showNAAsk, setShowNAAsk] = useState(true);
+  const [statusFilter, setStatusFilter] = useState('');
 
-export default ForexTable;
+  useEffect(() => {
+    const filtered = data.filter((event) => {
+      const statusMatch = statusFilter === '' || event.QuoteStatus === statusFilter;
+      const showMatchPosition = showNAPosition || event.Position !== 'NA';
+      const showMatchBid = showNABid || event.Bid !== 'NA';
+      const showMatchAsk = showNAAsk || event.Ask !== 'NA';
+
+      return statusMatch && showMatchPosition && showMatchBid && showMatchAsk;
+    });
+
+    setFilteredData(filtered);
+  }, [data, statusFilter, showNAPosition, showNABid, showNAAsk]);
+
+  const getCellStyle = (quoteStatus) => {
+    let color;
+    switch (quoteStatus) {
+      case 'TRADABLE':
+        color = '#00A86B'; // Green color code
+        break;
+      case 'EXCEPTION':
+        color = '#E61030'; // Red color code
+        break;
+      case 'NON-TRADABLE':
+        color = '#FFA500'; // Orange color code
+        break;
+      default:
+        color = '#000000'; // Default color code
+        break;
+    }
+    return { backgroundColor: color, color: 'white' };
+  };
+
+  return (
+    <div>
+      <div className="filters">
+        <label className="filter-label">
+          Status: 
+          <select
+            className="filter-select"
+            value={statusFilter}
+            onChange={(e) => setStatusFilter(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="TRADABLE">Tradable</option>
+            <option value="EXCEPTION">Exception</option>
+            <option value="NON-TRADABLE">Non-Tradable</option>
+          </select>
+        </label>
+        <label className="filter-label">
+          Position:
+          <select
+            className="filter-select"
+            value={showNAPosition ? 'all' : 'filtered'}
+            onChange={(e) => setShowNAPosition(e.target.value === 'all')}
+          >
+            <option value="all">All</option>
+            <option value="filtered">Filtered</option>
+          </select>
+        </label>
+        <label className="filter-label">
+          Bid:
+          <select
+            className="filter-select"
+            value={showNABid ? 'all' : 'filtered'}
+            onChange={(e) => setShowNABid(e.target.value === 'all')}
+          >
+            <option value="all">All</option>
+            <option value="filtered">Filtered</option>
+          </select>
+        </label>
+        <label className="filter-label">
+          Ask:
+          <select
+            className="filter-select"
+            value={showNAAsk ? 'all' : 'filtered'}
+            onChange={(e) => setShowNAAsk(e.target.value === 'all')}
+          >
+            <option value="all">All</option>
+            <option value="filtered">Filtered</option>
+          </select>
+        </label>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Currency</th>
+            <th>Tenor</th>
+            <th>Position</th>
+            <th>Bid</th>
+            <th>Ask</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((event, index) => (
+            <tr key={index}>
+              <td>{event.Ccy}</td>
+              <td>{event.Tenor}</td>
+              <td>{showNAPosition ? event.Position : event.Position !== 'NA' ? event.Position : ''}</td>
+              <td>{showNABid ? event.Bid : event.Bid !== 'NA' ? event.Bid : ''}</td>
+              <td>{showNAAsk ? event.Ask : event.Ask !== 'NA' ? event.Ask : ''}</td>
+              <td style={getCellStyle(event.QuoteStatus)}>{event.QuoteStatus}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+          }
+          export default ForexTable;
